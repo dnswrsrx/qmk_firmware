2562ec7e1fca092077d7dec622f1fadbf7d109cf
dnswrsrx <xtnct@dnswrsrx.com>
2026-02-15 16:44:14 -0500

    Call process_record_user within keyboard layer of process_record
    Following the pattern ffkeebs/puca
    
    Additional function to override key status call of MO(2) / MO(3) that's
    baked into the microcontroller firmware. Running with my own branch and
    fork here as I'm not sure how to do this well and don't want to commit
    it to carlosedp's repo to make it "official".

--- keyboards/chosfox/geonixr2/geonixr2.c
+++ keyboards/chosfox/geonixr2/geonixr2.c
@@ -92,5 +92,8 @@ void keyboard_post_init_kb(void) {
 }
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) {
+        return false;
+    }
     return kb_process_record_common(keycode, record);
 }
